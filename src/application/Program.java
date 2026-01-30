796cf8789a04557ee97e4fab2d3d1c37790261af
Joao Alexandre <jo.alexandre.ber@gmail.com>
2026-01-30 11:14:39 -0300

    Very bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Digite o numero do quarto: ");
+        int roomNumber = Integer.parseInt(sc.nextLine());
+        System.out.print("Data de check-in (dd/MM/yyyy): ");
+        Date checkin = sdf.parse(sc.nextLine());
+        System.out.print("Data de check-out (dd/MM/yyyy): ");
+        Date checkout = sdf.parse(sc.nextLine());
+
+        while(!checkout.after(checkin)) {
+            System.out.println("Erro na reserva: A data de check-out deve ser depois da data de check-in");
+            System.out.print("Data de check-in (dd/MM/yyyy): ");
+            checkin = sdf.parse(sc.nextLine());
+            System.out.print("Data de check-out (dd/MM/yyyy): ");
+            checkout = sdf.parse(sc.nextLine());
+        }
+
+        Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+        System.out.println(reservation);
+
+
+        System.out.println("Digite os dados para atualizar a reserva:");
+        System.out.print("Data de check-in (dd/MM/yyyy): ");
+        checkin = sdf.parse(sc.nextLine());
+        System.out.print("Data de check-out (dd/MM/yyyy): ");
+        checkout = sdf.parse(sc.nextLine());
+
+        Date now = new Date();
+        if(checkin.before(now) || checkout.before(now)) {
+            System.out.println("Erro na reserva: As datas de reserva para atualização devem ser datas futuras");
+        } else if(!checkout.after(checkin)) {
+            System.out.println("Erro na reserva: A data de check-out deve ser depois da data de check-in");
+        } else {
+            reservation.updateDates(checkin, checkout);
+            System.out.println(reservation);
+        }
+    }
+}
